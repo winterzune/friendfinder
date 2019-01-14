e63f129942d94b9a8f4ed3ddd819f15806da479d
Theo Elnecave <theo@theos-mbp.attlocal.net>
2019-01-13 20:25:39 -0500

    more routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+// ===============================================================================
+// this is the route life
+
+var path = require("path");
+
+module.exports = function(app) {
+
+    app.get("/home", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html.html"));
+      });
+    
+      app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+      });
+
+// If no matching route is found default to home
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
